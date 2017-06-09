425bd35c5ab5ece2ff2b77b9723d3ab9157f3ad7
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2017-06-09 09:42:20 +0200

    [intel-compilers]: package for version 2017

---
+++ ciment/intel/2017.nix
@@ -0,0 +1,38 @@
+{ stdenv, fetchurl, glibc, gcc }:
+
+stdenv.mkDerivation rec {
+  version = "2017";
+  name = "intel-compilers-${version}";
+  sourceRoot = "/scratch/intel/${version}";
+
+  buildInputs = [ glibc gcc ];
+
+  phases = [ "installPhase" "fixupPhase" "installCheckPhase" "distPhase" ];
+
+  installPhase = ''
+     cp -a $sourceRoot $out
+     # Fixing man path
+     rm -f $out/man
+     mkdir -p $out/share
+     ln -s ../compilers_and_libraries/linux/man/common $out/share/man
+  '';
+
+  postFixup = ''
+    echo "Fixing rights..."
+    chmod u+w -R $out
+    echo "Patching rpath and interpreter..."
+    find $out -type f -exec $SHELL -c 'patchelf --set-interpreter $(echo ${glibc}/lib/ld-linux*.so.2) --set-rpath ${glibc}/lib:${gcc.cc}/lib:${gcc.cc.lib}/lib:$out/lib/intel64:$out/compilers_and_libraries_2017.4.196/linux/bin/intel64 2>/dev/null {}' \;
+    echo "Fixing path into scripts..."
+    for file in `grep -l -r "$sourceRoot" $out`
+    do
+      sed -e "s,$sourceRoot,$out,g" -i $file
+    done 
+  '';
+
+  meta = {
+    description = "Intel compilers and libraries 2017";
+    maintainers = [ stdenv.lib.maintainers.bzizou ];
+    platforms = stdenv.lib.platforms.linux;
+  };
+}
+
