477f909d864a7c971acafbc9fedb4c3bf05df19b
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2017-07-06 12:09:59 +0200

    [gildas]: feb17d -> jul17a + fixes from nixpkgs

--- ciment/gildas/default.nix
+++ ciment/gildas/default.nix
@@ -1,17 +1,20 @@
 { stdenv, fetchurl, gtk2 , pkgconfig , python27 , gfortran , python27Packages , lesstif , cfitsio , getopt , perl , groff }:
 
 stdenv.mkDerivation rec {
-  version = "feb17d";
+  srcVersion = "jul17a";
+  version = "20170701_d";
   name = "gildas-${version}";
 
   src = fetchurl {
-    url = "http://www.iram.fr/~gildas/dist/gildas-src-${version}.tar.gz";
-    sha256 = "1b7rh3jbjvrpxqxf3ync3693daax429mwf40y8gsb3cb5d97v57a";
+    url = "http://www.iram.fr/~gildas/dist/gildas-src-${srcVersion}.tar.gz";
+    sha256 = "060vha4af2xamk0fvriqr6rgq4zayy4k6l5y6kj3h5cv9b4yiqzq";
   };
 
   enableParallelBuilding = false;
 
-  buildInputs = [ gtk2 pkgconfig python27 gfortran python27Packages.numpy lesstif cfitsio getopt perl groff ];
+  nativeBuildInputs = [ pkgconfig groff perl getopt gfortran python27 python27Packages.numpy ];
+
+  buildInputs = [ gtk2 lesstif cfitsio ];
 
   patches = [ ./format-security.patch ./wrapper.patch ];
 
@@ -25,19 +28,19 @@ stdenv.mkDerivation rec {
 
   installPhase=''
     mkdir -p $out/bin
-    cp -a ../gildas-exe-${version}/* $out
+    cp -a ../gildas-exe-${srcVersion}/* $out
     mv $out/$GAG_EXEC_SYSTEM $out/libexec
     cp admin/wrapper.sh $out/bin/gildas-wrapper.sh
     chmod 755 $out/bin/gildas-wrapper.sh
     for i in $out/libexec/bin/* ; do
-      ln -s $out/bin/gildas-wrapper.sh $out/bin/`basename $i`
+      ln -s $out/bin/gildas-wrapper.sh $out/bin/$(basename "$i")
     done
   '';
 
   meta = {
     description = "Radioastronomy data analysis software";
     longDescription = ''
-      GILDAS is a collection of state-of-the-art softwares
+      GILDAS is a collection of state-of-the-art software
       oriented toward (sub-)millimeter radioastronomical
       applications (either single-dish or interferometer).
       It is daily used to reduce all data acquired with the
