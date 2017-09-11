1129f9bc1fc0edd7293a8bdcf7dc4968ca04be6a
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2017-09-11 08:58:01 +0200

    petsc: more enabled features thanks to @knedlsepp
    Also fixed (tests now passing), using openblasCompat, thanks to
    @knedlsepp too

--- ciment/petsc/default.nix
+++ ciment/petsc/default.nix
@@ -1,11 +1,17 @@
 { stdenv
 , fetchurl
-, openblas
+, openblasCompat
 , python
 , gfortran
 , openmpi
 , tcsh
 , valgrind
+, suitesparse
+, hwloc
+, metis
+, superlu
+, hdf5
+, netcdf
 }:
 
 stdenv.mkDerivation rec {
@@ -19,31 +25,55 @@ stdenv.mkDerivation rec {
 
   patches = [ ./petscmpiexec.patch ];
 
-  nativeBuildInputs = [ openblas python openmpi tcsh valgrind gfortran ];
+  buildInputs = [ 
+    openblasCompat
+    python
+    gfortran
+    openmpi
+    tcsh
+    valgrind
+    suitesparse
+    hwloc
+    metis
+    superlu
+    hdf5
+    netcdf
+  ];
 
   enableParallelBuilding = true;
 
   preConfigure = ''
     patchShebangs .
-    configureFlagsArray=(
-      $configureFlagsArray
-      "--with-scalar-type=complex"
-      "--with-mpi=1"
-      "--with-mpi-dir=${openmpi}"
-      "--with-blas-lapack-lib=${openblas}/lib/libopenblas.a"
-      "--with-valgrind=1"
-      "--with-valgrind-dir=${valgrind}"
-    )
   '';
 
+  configureFlags = let
+    sharedLibraryExtension = if stdenv.isDarwin then ".dylib" else ".so";
+  in [
+      "--with-scalar-type=complex"
+      "--with-debugging=0"
+      "--with-mpi=1"
+      # PETSc is not threadsafe, disable pthread/openmp (see http://www.mcs.anl.gov/petsc/miscellaneous/petscthreads.html)
+      "--with-pthread=0"
+      "--with-openmp=0"
+      "--with-ssl=0"
+      "--with-x=0"
+      "--with-mpi-dir=${openmpi}"
+      "--with-blas-lapack-lib=${openblasCompat}/lib/libopenblas${sharedLibraryExtension}"
+      "--with-valgrind=1"
+      "--with-valgrind-dir=${valgrind}"
+      "--with-suitesparse-dir=${suitesparse}"
+      "--with-hwloc-dir=${hwloc}"
+      "--with-netcdf-dir=${netcdf}"
+      "--with-hdf5-dir=${hdf5}"
+      "--with-superlu-dir=${superlu}"
+      "--with-metis-dir=${metis}"
+  ];
 
   postInstall = ''
     rm $out/bin/popup
     rm $out/bin/uncrustify.cfg
     rm -rf $out/bin/win32fe
   '';
-
-  checkPhase = "make test";
 
   doCheck = true;
 
