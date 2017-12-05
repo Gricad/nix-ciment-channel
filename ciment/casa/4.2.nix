db4e055fe2de20861ba02aa1cbcca5143ad2fca2
Bruno Bzeznik <Bruno.Bzeznik@imag.fr>
2017-12-05 15:44:04 +0100

    casa42: removed openssl dep

--- ciment/casa/4.2.nix
+++ ciment/casa/4.2.nix
@@ -1,4 +1,4 @@
-{ stdenv, fetchurl, glibc, perl, python, xorg, zlib, gcc45, fontconfig, freetype, glib, libselinux, libxml2, sqlite, expat, bzip2, libkrb5, gdbm, e2fsprogs, openssl }:
+{ stdenv, fetchurl, glibc, perl, python, xorg, zlib, gcc45, fontconfig, freetype, glib, libselinux, libxml2, sqlite, expat, bzip2, libkrb5, gdbm, e2fsprogs }:
 
 stdenv.mkDerivation rec {
   version = "4.2";
@@ -9,7 +9,7 @@ stdenv.mkDerivation rec {
       sha256 = "0hax0f8niplwd35d1bbq2050hg1ggwv8sclxckkb161fnhd7raig"; 
     };                                                                 
 
-    nativeBuildInputs = [ glibc perl python xorg.libSM xorg.libICE xorg.libX11 xorg.libXext xorg.libXi xorg.libXrender xorg.libXrandr xorg.libXfixes xorg.libXcursor xorg.libXinerama xorg.libXft zlib gcc45 fontconfig glib freetype libselinux libxml2 sqlite expat xorg.libxcb bzip2 libkrb5 gdbm e2fsprogs openssl ];
+  nativeBuildInputs = [ glibc perl python xorg.libSM xorg.libICE xorg.libX11 xorg.libXext xorg.libXi xorg.libXrender xorg.libXrandr xorg.libXfixes xorg.libXcursor xorg.libXinerama xorg.libXft zlib gcc45 fontconfig glib freetype libselinux libxml2 sqlite expat xorg.libxcb bzip2 libkrb5 gdbm e2fsprogs ];
 
   phases = [ "unpackPhase" "installPhase" "fixupPhase" "installCheckPhase" "distPhase" ];
 
@@ -30,7 +30,7 @@ stdenv.mkDerivation rec {
     echo "Fixing rights..."
     chmod u+w -R $out
     echo "Patching rpath and interpreter..."
-    find $out -type f ! -name "libgomp.*" ! -name "_hashlib.so" -exec $SHELL -c "patchelf --set-rpath ${openssl.out}/lib:${glibc}/lib:$out/lib64:${gcc45.cc}/lib:${gcc45.cc.lib}/lib:${zlib}/lib:${glib}/lib:${freetype}/lib:${xorg.libSM}/lib:${xorg.libICE}/lib:${xorg.libX11}/lib:${xorg.libXext}/lib:${xorg.libXi}/lib:${xorg.libXrender}/lib:${xorg.libXrandr}/lib:${xorg.libXfixes}/lib:${xorg.libXcursor}/lib:${xorg.libXinerama}/lib:${xorg.libxcb}/lib:${xorg.libXft}/lib:${fontconfig.lib}/lib:${libselinux}/lib:${libxml2.out}/lib:${sqlite.out}/lib:${expat}/lib:$out/etc/carta/lib:${bzip2.out}/lib:${libkrb5}/lib:${gdbm}/lib:${e2fsprogs.out}/lib '{}' 2>/dev/null" \;
+    find $out -type f ! -name "libgomp.*" ! -name "_hashlib.so" -exec $SHELL -c "patchelf --set-rpath ${glibc}/lib:$out/lib64:${gcc45.cc}/lib:${gcc45.cc.lib}/lib:${zlib}/lib:${glib}/lib:${freetype}/lib:${xorg.libSM}/lib:${xorg.libICE}/lib:${xorg.libX11}/lib:${xorg.libXext}/lib:${xorg.libXi}/lib:${xorg.libXrender}/lib:${xorg.libXrandr}/lib:${xorg.libXfixes}/lib:${xorg.libXcursor}/lib:${xorg.libXinerama}/lib:${xorg.libxcb}/lib:${xorg.libXft}/lib:${fontconfig.lib}/lib:${libselinux}/lib:${libxml2.out}/lib:${sqlite.out}/lib:${expat}/lib:$out/etc/carta/lib:${bzip2.out}/lib:${libkrb5}/lib:${gdbm}/lib:${e2fsprogs.out}/lib '{}' 2>/dev/null" \;
     find $out -type f -exec $SHELL -c "patchelf --set-interpreter $(echo ${glibc}/lib/ld-linux*.so.2) '{}' 2>/dev/null" \;
     patchelf --set-rpath $out/lib64 $out/lib64/python2.7/lib-dynload/_hashlib.so
     echo "Fixing path into scripts..."
