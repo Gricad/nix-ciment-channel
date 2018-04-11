b806170699efb557e028b0c4b4201c8182ddc936
Bruno Bzeznik <Bruno.Bzeznik@univ-grenoble-alpes.fr>
2018-04-11 14:40:33 +0200

    Trying to resolve python dependencies locally (but fail)

--- ciment/python-modules/spectral-cube/default.nix
+++ ciment/python-modules/spectral-cube/default.nix
@@ -1,13 +1,13 @@
-{ stdenv, buildPythonPackage, fetchPypi, numpy, astropy, radio_beam }:
+{ stdenv, pythonPackages, radio-beam }:
 
-buildPythonPackage rec {
+pythonPackages.buildPythonPackage rec {
   pname = "spectral-cube";
   version = "0.4.3";
   name = "${pname}-${version}";
 
-  propagateBuildInputs= [ numpy astropy radio_beam ];
+  propagateBuildInputs= [ pythonPackages.numpy pythonPackages.astropy radio-beam ];
 
-  src = fetchPypi {
+  src = pythonPackages.fetchPypi {
     inherit pname version;
     sha256 = "057g3mzlg5cy4wg2hh3p6gssn93rs6i7pswzhldvcq4k8m8hsl3b";
   };
